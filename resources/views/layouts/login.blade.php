e883761e2ea34be4ec4a8c3870e175f417663c98
Frank Hesse <45906155+metalsonic21@users.noreply.github.com>
2019-12-01 22:53:48 -0400

    just changed login's title

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -43,7 +43,7 @@
   <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
     <div class="container">
       <div class="navbar-wrapper">
-        <a class="navbar-brand" href="#pablo">Los Clubes de Lectura</a>
+        <a class="navbar-brand" href="#pablo">LOS CLUBES DE LECTURA</a>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
         <span class="sr-only">Toggle navigation</span>
