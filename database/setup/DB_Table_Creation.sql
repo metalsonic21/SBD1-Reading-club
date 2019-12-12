3afef6495da21c943437af2c66f9514b04320379
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2019-12-12 19:11:44 -0400

    changed from varchar(20) to varchar(40) in group name

--- database/setup/DB_Table_Creation.sql
+++ database/setup/DB_Table_Creation.sql
@@ -27,7 +27,7 @@ CREATE TABLE SJL_lectores (
 CREATE TABLE SJL_grupos_lectura (
     id                	SERIAL NOT NULL,
     id_club           	INTEGER NOT NULL,
-    nom               	VARCHAR(20) NOT NULL,
+    nom               	VARCHAR(40) NOT NULL,
     tipo 				VARCHAR(2) NOT NULL,
     dia_sem           	INTEGER NOT NULL,
     hora_i            	TIME NOT NULL,
