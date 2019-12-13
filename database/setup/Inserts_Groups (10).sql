e571e56cdc2fe95065df066491ab5bacf39036e7
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2019-12-12 20:01:24 -0400

    Several inserts for groups

--- database/setup/Inserts_Groups (10).sql
+++ database/setup/Inserts_Groups (10).sql
@@ -1,3 +1,99 @@
 INSERT INTO sjl_grupos_lectura(
 	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
 	VALUES (1, 1, 'Nohidea', 'A', 2, '13:00:00', '15:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (2, 1, 'Novo Armor', 'J', 5, '11:00:00', '13:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (3, 1, 'MAPO Fan Club', 'A', 4, '14:00:00', '16:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (4, 1, 'G#', 'A', 4, '09:00:00', '11:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (5, 1, 'SchemaPosse', 'N', 2, '11:00:00', '13:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (6, 2, 'Infinity', 'N', 2, '10:00:00', '12:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (7, 2, 'Doomshop', 'A', 4, '13:00:00', '15:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (8, 2, 'Killer Bees', 'J', 3, '08:00:00', '10:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (9, 2, 'The Coven', 'J', 2, '10:00:00', '12:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (10, 2, 'Fire Emblem', 'J', 2, '08:00:00', '10:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (11, 3, 'Russian Doomers', 'A', 5, '15:00:00', '17:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (12, 3, 'El Perla Negra', 'J', 4, '15:00:00', '17:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (13, 3, 'Akoza', 'N', 4, '14:00:00', '16:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (14, 3, 'Axius Link', 'A', 4, '12:00:00', '14:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (15, 3, 'Dori Me', 'J', 2, '12:00:00', '14:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (16, 4, 'Bepis', 'A', 3, '08:00:00', '10:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (17, 4, 'Peña Española', 'N', 2, '07:00:00', '09:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (18, 4, 'Peña Española', 'N', 4, '10:00:00', '12:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (19, 4, 'Notepad Programmers', 'J', 3, '13:00:00', '15:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (20, 4, 'Secret DooR?', 'A', 4, '15:00:00', '17:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (21, 5, 'Power Ranger Rojo', 'J', 4, '11:00:00', '13:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (22, 5, 'Book Row', 'N', 2, '09:00:00', '11:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (23, 5, 'Happy Team', 'A', 2, '07:00:00', '09:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (24, 5, 'Windows 96', 'N', 5, '15:00:00', '17:00:00');
+
+INSERT INTO sjl_grupos_lectura(
+	id, id_club, nom, tipo, dia_sem, hora_i, hora_f)
+	VALUES (25, 5, 'Macintosh Plus', 'A', 5, '13:00:00', '15:00:00');
